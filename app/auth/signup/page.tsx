7aef93d85f87d3f1b001cba77ce329d9c4b07110
nishit-27 <nishitiitr27@gmail.com>
2025-03-03 14:57:53 +0530

    auth

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,7 +1,22 @@
+"use client"
+import axios from "axios"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
 export default function SignUp() {
+    const [userName, setUserName] = useState("")
+    const [password, setPassword] = useState("")
+    const router = useRouter()
     return(
     <div>
-        signup page
+        <input type = "text" placeholder="username" onChange={(e)=> setUserName(e.target.value)}></input>
+        <input type = "password" placeholder="password" onChange={(e) => setPassword(e.target.value)}></input>
+        <button onClick={async () => {
+           await axios.post("http://localhost:3000/api/v1/signup",{
+                userName,
+                password
+            });
+            router.push("/auth/signin")
+        }}> signup</button>
     </div>
     )
    
