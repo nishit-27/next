ccbd2f1a634f19fc8d1e75efee492c9290af6998
nishit-27 <nishitiitr27@gmail.com>
2025-03-03 18:43:38 +0530

    added prisma

--- app/api/v1/signup/route.ts
+++ app/api/v1/signup/route.ts
@@ -1,8 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
+const prismaClient = new PrismaClient()
 export async function POST(req:NextRequest) {
     const data = await req.json()
     console.log(data)
+    await prismaClient.user.create({
+        data: {
+            username : data.userName,
+            password : data.password
+        }
+    })
     return NextResponse.json({
         message: "you have successfully signup"
     })
